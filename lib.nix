415802fd971557fefc332d295527d61e304687e9
Jonathan Ringer <jonringer117@gmail.com>
2024-09-08 16:12:02 -0700

    Bump lib pin

--- lib.nix
+++ lib.nix
@@ -1,7 +1,7 @@
 let
   lib = import (builtins.fetchGit {
   url = "https://github.com/jonringer/nix-lib.git";
-  rev = "9ec8ccc745ea8d3e767e2d4db4f62b03aea11135";
+  rev = "c19c816e39d14a60dd368d601aa9b389b09d0bbb";
 });
 in lib.extend(self: _: {
   systems = import ./systems { lib = self; };
